03a534fff68644e0452a23d57f7720935a00dcfe
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-07-20 16:46:33 -0300

    Feat: Database created and structured

---
+++ database/database.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
+const connection = new Sequelize(dbName, dbUser, dbPass, {
+  host: "localhost",
+  dialect: "mysql"
+});
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const connection = require("./database/database");
 
+// View Engine
 app.set('view engine', 'ejs');
 
+// Static
 app.use(express.static("public"));
 
+// Body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Database
+connection.authenticate()
+.then(() => {
+  console.log("Connected successfully!");
+}).catch((error) => {
+  console.log("Error: " + error);
+});
 
 app.get("/", function(req, res){
   res.render("index");
