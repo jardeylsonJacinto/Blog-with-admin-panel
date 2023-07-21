b7f6d36750ac2909f62e691397d6316aef71d551
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-07-21 20:15:20 -0300

    Feat: Model article created

--- articles/Article.js
+++ articles/Article.js
@@ -1,14 +1,17 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/database");
 
-const Category = connection.define("categories", {
+const Article = connection.define("articles", {
   title:{
     type: Sequelize.STRING,
     allowNull: false
   }, slug:{
     type: Sequelize.STRING,
     allowNull: false
+  }, body: {
+    type: Sequelize.TEXT,
+    allowNull: false
   }
 })
 
-module.exports = Category;
+module.exports = Article;
